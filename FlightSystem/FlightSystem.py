dc072ef3ef034a43cc9a1d5ab0bf09a07c3d6d86
Grace <gracebolutife@gmail.com>
2021-01-21 18:49:55 +0100

    flight system

---
+++ FlightSystem/FlightSystem.py
@@ -0,0 +1,35 @@
+class AirPlane():
+
+    passengers = {}
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def set_plane_capacity(self, capacity):
+        self.capacity = capacity
+
+    def get_place_capacity():
+        return self.capacity
+
+    def is_seat_available():
+        if self.capacity - passengers == 0:
+            return "No more seats available" 
+
+
+class Customer():
+    def __init__(self, name):
+        self.name = name
+
+    def book_a_seat():
+
+
+
+    def fill_booking_information(self, location):
+        self.location = location
+
+    def get_booking_information():
+        information = [
+            "name" : self.name
+            "location" : self.location
+        ]
+
+        
